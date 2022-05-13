987e11e61a609768f9e3c9e21ebe6b7a842d43e1
the_mjl <michaelqmichael@gmail.com>
2022-05-13 17:49:30 -0400

    setting up initial checkin to build what we have out-of-box.

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-gcc -c linenoise/linenoise.c
-g++ -c LineReader.cpp
-g++ -c Shell.cpp
-g++ -o testConsole *.o TestConsole.cpp
+CFLAGS += "-Iinc"
+
+gcc $CFLAGS -c other/linenoise/linenoise.c
+
+g++ $CFLAGS -c src/LineReader.cpp
+g++ $CFLAGS -c src/Shell.cpp
+
+# main test driver
+g++ $CFLAGS -c TestConsole.cpp
+
+#g++ $CFLAGS -o testConsole *.o TestConsole.cpp
+
+
