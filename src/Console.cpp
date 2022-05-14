87fe082faa583ed6fcd84d1a22449fb65099a359
the_mjl <michaelqmichael@gmail.com>
2022-05-13 20:57:57 -0400

    templates funtime

--- src/Console.cpp
+++ src/Console.cpp
@@ -4,13 +4,13 @@
 #include "Console.h"
 
 ///< Exit the Console.
-
+template <class T>
 void Console::Exit(void) {
 
 }
 
 ///< Begin Console run-loop.
-
+template <class T>
 void Console::Run(void) {
 
 }
