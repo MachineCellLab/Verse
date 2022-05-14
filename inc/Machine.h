ddb7767c64f42e6053eab78715c73d71e19918f3
the_mjl <michaelqmichael@gmail.com>
2022-05-13 20:28:18 -0400

    more cleanup

--- inc/Machine.h
+++ inc/Machine.h
@@ -5,22 +5,29 @@
 #ifndef _machine_h_
 #define _machine_h_
 
-#include <ctring>
+#include <cstring>
 
 #include <string>
 #include <list>
 #include <map>
 #include <vector>
 
+using namespace std;
+
 /* Boundary types... 
 */
 typedef size_t Index;
 typedef size_t Count;
+typedef size_t Size;
 
 typedef ssize_t Integer;
 
 typedef double Real;
 
+typedef string Text;
+
+/* internal Token types
+*/
 typedef std::string Token;
 
 typedef std::list<Token> TokenList;
